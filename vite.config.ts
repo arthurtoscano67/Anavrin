e56ecb49a614b25ee30c9e3a89ed7b63b06a7a80
Arthur Toscano <hartygt@yahoo.com>
2026-03-05 16:40:24 -0800

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/Anavrin/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5174,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: 5174,
-  },
-});
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
+})
